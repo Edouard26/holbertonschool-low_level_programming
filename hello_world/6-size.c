8ed3acda4ad036590de56ef3529e9f7e04bbfa05
Edouard26 <edouard.halimi@gmail.com>
2024-02-20 14:22:19 +0000

    update

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,19 +5,15 @@
  *Return: (int) Success
  */
 
-int main()
+int main(void)
 
 {
 	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of short: %zu bytes\n", sizeof(short));
 	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of long: %zu bytes\n", sizeof(long int));
+	printf("Size of long long: %zu bytes\n", sizeof(long long int));
 	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of long double: %zu bytes\n", sizeof(long double));
-	printf("Size of pointer: %zu bytes\n", sizeof(void*));
-    
-    return 0;
+
+	return (0);
 }
 
