989846647e401389f4b9aa2d9a08b64a9b9ae819
Edouard26 <edouard.halimi@gmail.com>
2024-02-21 16:02:23 +0000

    update

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Start program
+ *
+ * Return: always (0) Success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		if (i == 'q' || i == 'e')
+
+		continue;
+	}
+		putchar(i);
+	putchar ('\n');
+	return (0);
+}
