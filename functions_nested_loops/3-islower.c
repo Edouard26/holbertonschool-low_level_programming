6ec206da47efdc133347d9cd78d90bb9afb78557
Edouard26 <edouard.halimi@gmail.com>
2024-02-23 14:02:48 +0000

    update

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _islower - lowercase
+ * Return: always (0) Success
+ */
+
+int _islower(int c)
+
+
+{
+		if (c >= 'a' && c <= 'z')
+		
+			return (1);
+
+		else
+			return (0);
+}
