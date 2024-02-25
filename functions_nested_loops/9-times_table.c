6b6a97a4313c7af748539256d95fb04b127f2e0d
Edouard26 <edouard.halimi@gmail.com>
2024-02-25 22:06:31 +0000

    Q9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,25 +5,24 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
-            if (column != 0)
-            {
-                _putchar(',');
-                if (product < 10)
-                    _putchar(' ');
-            }
-            if (product < 10)
-                _putchar(' ');
-            else
-                _putchar((product / 10) + '0');
-            _putchar((product % 10) + '0');
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			if (column != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (product < 10)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
