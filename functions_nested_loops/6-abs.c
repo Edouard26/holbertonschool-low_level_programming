5d63100f870abf5a2625cdf710fdcd9f630ddbab
Edouard26 <edouard.halimi@gmail.com>
2024-02-25 20:21:52 +0000

    6-abs - absolut number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _abs(int) - computes the absolute value of an integer
+ *
+ * Return: Always (0) Success
+ */
+
+int _abs(int i)
+
+{
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+
+	else
+	{
+		return (-i);
+	}
+}
