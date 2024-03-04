3df683a764ab1339ad1524fda8c338c55bd97232
Edouard26 <edouard.halimi@gmail.com>
2024-03-04 14:33:25 +0100

    add 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - settings
+ * @*s: point to the memory area to fill
+ * @b: constant byte value
+ * @n: number of bytes to be filled
+ * Return: Void.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int v;
+
+	for (v = 0 ; v < n ; v++)
+
+		s[v] = b;
+
+
+	return (0);
+}
