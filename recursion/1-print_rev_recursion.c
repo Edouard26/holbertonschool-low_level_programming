617f29b4a7d739fc24a6586565eae92044e7a58b
Edouard26 <edouard.halimi@gmail.com>
2024-03-06 16:22:21 +0100

    correction Q.1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: string
+ * Return: void
  */
 
 
@@ -10,7 +11,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 	 _print_rev_recursion(s + 1);
