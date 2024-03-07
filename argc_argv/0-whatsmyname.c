c618560555f7827ade05c6a3c659a4401fcb7b70
Edouard26 <edouard.halimi@gmail.com>
2024-03-07 15:25:14 +0100

    add file 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function that print the name of the function itself
+ * @argc: number of things into the command line
+ * @argv: array that hold the string value of the things that we entered
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	
+		return (0);
+}
