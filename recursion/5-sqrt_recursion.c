bb647d10993e7e5648ed6a10f42f7ab080a4d751
Edouard26 <edouard.halimi@gmail.com>
2024-03-07 10:44:42 +0100

    add file 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * _sqrt_recursion - Wrapper function to call the recursive function
 *@n: The number to find the square root of
@@ -15,17 +14,17 @@ int _sqrt_recursion(int n)
 }
 
 /**
- *_sqrt_recursion_2 - Find the square root of a number recursively
- *@n: The number to find the square root of
- *@s: The current guess for the square root
- *Return: The sqaure root of n if found, -1 otherwise
+*_sqrt_recursion_2 - Find the square root of a number recursively
+*@n: The number to find the square root of
+*@s: The current guess for the square root
+*Return: The sqaure root of n if found, -1 otherwise
 */
 
 int _sqrt_recursion_2(int n, int s)
 {
-	if (s * s == n)
+	if ((s * s) == n)
 		return (s);
-	if (s * s > n)
+	if ((s * s) > n)
 		return (-1);
 	return (_sqrt_recursion_2(n, (s + 1)));
 }
