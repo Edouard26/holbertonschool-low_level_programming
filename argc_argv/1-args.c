fa1b2e8ef4159c0821520445554b8d56506667cf
Edouard26 <edouard.halimi@gmail.com>
2024-03-07 15:49:38 +0100

    correcting betty style for Q.0 and Q.1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,12 +3,15 @@
 /**
  * main - print the number of argc
  * @argc: argc
- * @*argv: argv
+ * @argv: argv
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	(void)*argv;
+
 	printf("%d\n", (argc - 1));
-    		return (0);
+
+		return (0);
 }
