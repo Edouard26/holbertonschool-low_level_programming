d9fcd59557e9d437c830558a4423f89a3f72632a
Edouard26 <edouard.halimi@gmail.com>
2024-03-13 11:51:14 +0100

    correction betty 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,19 +10,19 @@
  */
 int *array_range(int min, int max)
 {
-    int *array, i, size;
+	int *array, i, size;
 
-    if (min > max)
-        return NULL;
+	if (min > max)
+		return (NULL);
 
-    size = max - min + 1;
+	size = max - min + 1;
 
-    array = malloc(size * sizeof(int));
-    if (array == NULL)
-        return NULL;
+	array = malloc(size * sizeof(int));
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        array[i] = min + i;
+	for (i = 0; i < size; i++)
+		array[i] = min + i;
 
-    return array;
+	return (array);
 }
