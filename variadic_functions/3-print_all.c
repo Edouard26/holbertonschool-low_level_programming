d252b7980235119fc1d8f4df5e3b2614710af7d4
Edouard26 <edouard.halimi@gmail.com>
2024-03-20 17:12:50 +0100

    Q.3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -67,7 +67,8 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, ii;
-	print_format_t types[] = {
+	print_format_t types[] = 
+	{
 		{'c', print_char},
 		{'i', print_integer},
 		{'f', print_float},
