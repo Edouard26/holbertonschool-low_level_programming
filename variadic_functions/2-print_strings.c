85e72474534c64b1a9ff1a53647cca0a687556a0
Edouard26 <edouard.halimi@gmail.com>
2024-03-20 16:54:06 +0100

    Q.3

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
 
 /**
  * print_strings - function that prints strings
- * @separator: string to be printed between the strings 
+ * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
  */
 
