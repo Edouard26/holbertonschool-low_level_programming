7627e679afb51cb2c6ec89fa3506c9966096ae18
Edouard26 <edouard.halimi@gmail.com>
2024-03-20 12:06:19 +0100

    correction 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,52 +1,35 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 
 /**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of strings containing the arguments
- * Return: Always 0
+ * main - Start program
+ * @argc: Nb of arguments
+ * @argv: Arguments
+ * Return: (int)
  */
-
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
-	int div = 1;
+	int a, b;
+	int (*f)(int a, int b);
+	int result = 0;
 
-	if (argc != 4) 
+	if (argc != 4)
 	{
-		char *error_message = "Error\n";
-		while (*error_message)
-			putchar(*(error_message++));
-		return (98);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	op_func = get_op_func(argv[2]);
-
-	if (op_func == NULL) {
-		char *error_message = "Error\n";
-		while (*error_message)
-			putchar(*(error_message++));
-		return (99);
+		printf("Error\n");
+		exit(98);
 	}
 
-	result = op_func(num1, num2);
-
-	if (result < 0) {
-		result = -result;
-		div = -1;
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
 	}
-	if (result > 9)
-		div *= 10;
-	if (result > 99)
-		div *= 10;
-	if (div > 1)
-		putchar((result / div) + '0');
-	putchar((result % 10) + '0');
-	putchar('\n');
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = f(a, b);
+
+	printf("%d\n", result);
 	return (0);
 }
