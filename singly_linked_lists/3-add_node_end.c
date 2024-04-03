5654d8a97df949f1e3dac4f373a678b789a3ed95
Edouard26 <edouard.halimi@gmail.com>
2024-04-03 16:30:08 +0200

    create 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,9 +8,12 @@
  * @str: The string to be duplicated and added to the list.
  * Return: The address of the new element, or NULL if it failed.
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *current;
+	list_t *new_node, *last_node;
+	unsigned int len = 0;
+	unsigned int i;
 
 	if (str == NULL)
 		return (NULL);
@@ -19,14 +22,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	while (str[len])
+		len++;
+	new_node->str = malloc(sizeof(char) * (len + 1));
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+	for (i = 0; i <= len; i++)
+		new_node->str[i] = str[i];
 
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -35,11 +42,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new_node);
 	}
 
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
+	last_node = *head;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
 
-	current->next = new_node;
+	last_node->next = new_node;
 
 	return (new_node);
 }
