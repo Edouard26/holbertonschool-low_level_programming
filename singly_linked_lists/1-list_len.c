8c4540fbb52f80dbd163543d5b89960cfc9e5b8b
Edouard26 <edouard.halimi@gmail.com>
2024-04-03 14:25:13 +0200

    correction betty of 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -5,12 +5,13 @@
  * list_len - returns the number of elements in a linked list
  * @list_t: list
  * @h: pointer of the head
+ * Return: count
  */
 
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *current = h;
+	const list_t *current = h
 
 	while (current != NULL)
 	{
@@ -18,5 +19,5 @@ size_t list_len(const list_t *h)
 		current = current->next;
 	}
 
-	return count;
+	return (count);
 }
