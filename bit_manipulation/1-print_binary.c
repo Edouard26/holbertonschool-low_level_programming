726e11ca1952ed1e001a97ebd571b2c151d3f54e
Edouard26 <edouard.halimi@gmail.com>
2024-04-06 04:56:07 +0200

    correction 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,7 +4,9 @@
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to print in binary
+ * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
